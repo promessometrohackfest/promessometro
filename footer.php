21548e0cc12f28f4193d828b8c1cede9e8d16beb
promessometrohackfest <44249931+promessometrohackfest@users.noreply.github.com>
2018-11-30 23:06:47 -0400

    ultima atualização

--- footer.php
+++ footer.php
@@ -2,10 +2,11 @@
 <div class="py-2 bg-dark" style="	">
     <div class="container">
       <div class="row">
-        <div class="col-md-4" style=""><img class="d-block img-fluid" src="images/rodape_logo_branca.svg" width="40%"></div>
+        <div class="col-md-4" style=""><img class="d-block img-fluid" src="images/rodape_logo_branca.svg" width="40%">
+              <li> <a class="nav-link" href="admin.php">Administração</a> </li>
+           </div>
         <div class="col-md-4" style="">
-          <p style="" class="mt-4 text-light">Copyright (C) <?php echo date('Y');?>Promessometro.</em>Todos os Direitos Reservados.</p>
-             <li> <a class="nav-link ml-5" href="admin.php">Administração</a> </li>
+          <p style="" class="mt-4 text-light">Copyright (C) <?php echo date('Y');?> Promessometro.</em> Todos os Direitos Reservados.</p>
         </div>
            
         <div class="col-md-4" style="">
@@ -13,10 +14,9 @@
 			<a href="http://facebook.com/" target="_blank"> <img class="ml-5" src="images/Ícone Facebook.svg" width="30"></a>
 			<a href="http://twitter.com" target="_blank"> <img class="mx-1" src="images/Ícone Twitter.svg" width="30"></a>
 			<a href="#" target="_blank"><img class="" src="images/Ícone Whatsapp.svg" width="30"></a>
+             <li> <a class="nav-link" href="politica.php">Politica de privacidade</a> </li>
         </div>
+     
+     </div>
       </div>
-    </div>
-  </div>
-	</footer> 
-</body>
-</html>
+     </footer> 
--- politica.php
+++ politica.php
@@ -1,5 +1,5 @@
 <?php
 	require_once 'Conn/conexao.php';
 	include('header2.php');
- 	include 'pages/governadores.php';
+ 	include 'pages/politica_pri.php';
 	include('footer.php');
